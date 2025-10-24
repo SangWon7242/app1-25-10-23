79cb08b45a7566b3929e83c72cd879fd2e88facf
SangWon7242 <gotkdqja@gmail.com>
2025-10-24 16:50:41 +0900

    work

---
+++ frontend/src/app/ClientPage.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ClientPage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <>
+      <h1>게시글 목록</h1>
+      <ul>
+        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+        {posts.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
